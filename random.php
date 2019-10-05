ede932c7656668c222f5aede12c7643d0f7b0148
Dipantan Chakraborty <sanju.mahinagar@gmail.com>
2019-10-05 21:19:04 +0530

    Random consignment number generation

--- random.php
+++ random.php
@@ -1,17 +1,14 @@
-
 <?php
 function getRandomString($length=16){
 	$characters='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
 	$string="";
-    $date = "-". date("Y/m/d");
+    $date = date("Y/m/d");
 	for($i=0;$i<$length;$i++){
 		$string.=$characters[mt_rand(0,strlen($characters)-1)];
-		$c="$string.$date";
+		$c="$string-$date";
 	}
 	return $c;
 }
-//echo getRandomString();
 ?>
 
 
